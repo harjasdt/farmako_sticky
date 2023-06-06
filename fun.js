b82d54548c689696b9d75330c5bbb92f951e647b
Harjaspreet <68768529+harjasdt@users.noreply.github.com>
2023-06-06 10:07:22 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,14 @@
+
+
+
+
+window.addEventListener("scroll", function(event){
+  var scroll=this.scrollY;
+
+
+  
+  document.getElementById("cord").innerHTML="Scroll Coordinates:-"+scroll;
+
+ 
+
+});
